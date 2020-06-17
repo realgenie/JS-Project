93fb8a1481193050533bc686e5f5ef6bab75fe51
realgenie <55521587+realgenie@users.noreply.github.com>
2020-06-18 06:17:39 +0900

    earth

--- earth/earth.js
+++ earth/earth.js
@@ -1,3 +1,4 @@
+  
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -5,32 +6,40 @@ var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var texture = new THREE.TextureLoader().load('earthmap1k.jpg');
-var geometry   = new THREE.SphereGeometry(5, 32, 32)
-var material  = new THREE.MeshPhongMaterial(
-    {
-        map: texture
-    }
-)
-var earth = new THREE.Mesh(geometry, material)
-scene.add(earth);
-
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-camera.position.set(-90,-60, 60);
+camera.position.set(60, 30, 35);
 controls.update();
 
-var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1);
-scene.add( light );
-
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.2);
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
 scene.add( directionalLight );
 
-var ambientLight = new THREE.AmbientLight(0x0f0f0f,3);
+var ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
 var light = new THREE.SpotLight(0xffffff, 0.5);
 light.position.set(50,0,200);
 scene.add(light);
+
+
+
+
+var earth = new THREE.Mesh(
+    texture = new THREE.TextureLoader().load('earthmap1k.jpg'),
+    bump = new THREE.TextureLoader().load('earthbump1k.jpg'),
+    spec = new THREE.TextureLoader().load('earthspec1k.jpg'),
+    geometry = new THREE.SphereGeometry(3, 32, 32),
+    material = new THREE.MeshPhongMaterial(
+        {
+            map: texture,
+            bumpMap: bump,
+            bumpScale: 0.08,
+            specularMap: spec
+        }
+    )
+);
+var earth = new THREE.Mesh(geometry, material)
+scene.add(earth);
+
 
 
 
