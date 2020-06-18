f0423ee9ec1a97be73f11c345c391a5151ea9785
realgenie <55521587+realgenie@users.noreply.github.com>
2020-06-19 01:14:42 +0900

    위치

--- earth/earth.js
+++ earth/earth.js
@@ -1,15 +1,20 @@
-  
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(150, 150, 150);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+scene.add(camera); //add?
+camera.position.set(0, 35, 70);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
 controls.update();
+
+
+
+var spotLight = new THREE.DirectionalLight(0xffffff);
+spotLight.position.set(50,50,50);
+scene.add(spotLight);
 
 var directionalLight = new THREE.DirectionalLight( 0xffffff);
 directionalLight.position.set(50,50,50);
@@ -18,14 +23,9 @@ scene.add( directionalLight );
 var ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-var light = new THREE.SpotLight(0xffffff, 0.5);
-light.position.set(50,0,200);
-//scene.add(light);
-
-
 var star = new THREE.Mesh(
     texture = new THREE.TextureLoader().load('starfield.png'),
-    geometry = new THREE.SphereGeometry(50, 64, 64), 
+    geometry = new THREE.SphereGeometry(50, 50, 50), 
     material = new THREE.MeshBasicMaterial(
         {
             map: texture,
@@ -40,24 +40,25 @@ var earth = new THREE.Mesh(
     texture = new THREE.TextureLoader().load('earthmap1k.jpg'),
     bump = new THREE.TextureLoader().load('earthbump1k.jpg'),
     spec = new THREE.TextureLoader().load('earthspec1k.jpg'),
-    geometry = new THREE.SphereGeometry(5, 32, 32),
+    geometry = new THREE.SphereGeometry(8, 50, 50),
     material = new THREE.MeshPhongMaterial(
         {
             map: texture,
             bumpMap: bump,
             bumpScale: 0.08,
             specularMap: spec,
-            shininess: 2
+            shininess: 3
         }
     )
 );
 var earth = new THREE.Mesh(geometry, material)
-earth.position.set(0, 35, 70);
 scene.add(earth);
+
+
 
 var clouds = new THREE.Mesh(
     texture = new THREE.TextureLoader().load('earthcloudmaptrans.jpg'),
-    cloudGeometry = new THREE.SphereGeometry(3.09,  32, 32),
+    cloudGeometry = new THREE.SphereGeometry(8.09, 50, 50),
     cloudMaterial = new THREE.MeshPhongMaterial(
         {
             map: texture,
@@ -73,7 +74,7 @@ earth.add(clouds);
 
 var sun = new THREE.Mesh(
     texture = new THREE.TextureLoader().load('sunmap.jpg'),
-    geometry = new THREE.SphereGeometry(3, 32, 32),
+    geometry = new THREE.SphereGeometry(5, 50, 50),
     material = new THREE.MeshPhongMaterial(
         {
             map: texture,
@@ -82,12 +83,29 @@ var sun = new THREE.Mesh(
     )
 );
 var sun = new THREE.Mesh(geometry, material)
-earth.position.set(0, 40, 65);
+sun.position.set(-40,0,0);
 scene.add(sun);
 
-var sunVec = new THREE.Vector3(0,0,0);
+var moon = new THREE.Mesh(
+    texture = new THREE.TextureLoader().load('moon_texture.jpg'),
+    bump = new THREE.TextureLoader().load('moonbump1k.jpg'),
+    geometry = new THREE.SphereGeometry(3, 50, 50),
+    material = new THREE.MeshPhongMaterial(
+        {
+            map: texture,
+            bumpMap: bump,
+            bumpScale: 0.08,
+         
+        }
+    )
+);
+var moon = new THREE.Mesh(geometry, material)
+moon.position.set(35,0,0);
+scene.add(moon);
 
-var r = 35;
+var earthVec = new THREE.Vector3(0,0,0);
+
+var r = 30;
 var theta = 0;
 var dTheta = 2 * Math.PI / 1000;
 
@@ -96,15 +114,14 @@ var dy = -.01;
 var dz = -.05;
 
 
-
 function animate() {
     requestAnimationFrame(animate); 
     earth.rotation.y -= 0.009;
     clouds.rotation.y -= 0.005
       
     theta += dTheta;
-    earth.position.x = r * Math.cos(theta);
-    earth.position.z = r * Math.sin(theta);
+    moon.position.x = r * Math.cos(theta);
+    moon.position.z = r * Math.sin(theta);
 
     if (camera.position.z < 0) {
         dx *= -1;
@@ -113,13 +130,13 @@ function animate() {
       camera.position.y += dy;
       camera.position.z += dz;
 
-      camera.lookAt(sunVec);
+      camera.lookAt(earthVec);
 
       if (camera.position.z < -100) {
         camera.position.set(0,35,70);
       }
 
-      camera.lookAt(sunVec);
+      camera.lookAt(earthVec);
 
     renderer.render(scene, camera);
 };
